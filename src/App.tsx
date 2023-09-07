809caded3457ce4ffb5fde72e06e69dcb1ff58fc
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2023-09-07 20:34:55 +0100

    bootstrap added

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // import { useState } from 'react'
 // import './App.css'
 
+
+import Navbar from './navbar';
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <header className="mt-3">
+      <header className="mt-4 border-bottom">
         <div>
-          <h1>ToDo</h1>
+          <h1 className="fw-bold m-0">ToDo</h1>
         </div>
         <div>
-
+          <Navbar />
         </div>
       </header>
       <main>
