02c82526710ab6c1a265e75adb1cf7e9ed55cbbf
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2023-09-07 19:50:39 +0100

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className="mt-3">
+        <div>
+          <h1>ToDo</h1>
+        </div>
+        <div>
+
+        </div>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
+}
+
+export default App
